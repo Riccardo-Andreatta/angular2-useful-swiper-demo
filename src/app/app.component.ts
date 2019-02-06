3498b9800b72a62df1b268a95e376cc44e0d3dca
Riccardo Andreatta <26876480+Riccardo-Andreatta@users.noreply.github.com>
2019-02-06 15:50:25 +0000

    added new vertical thumbnails navigation example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,16 @@ import { SwiperComponent } from 'angular2-useful-swiper';
 })
 export class AppComponent {
     public images: Array<string> = [
-        'https://picsum.photos/700/250/?image=20',
-        'https://picsum.photos/700/250/?image=21',
+        'https://picsum.photos/700/250/?image=27',
         'https://picsum.photos/700/250/?image=22',
+        'https://picsum.photos/700/250/?image=61',
         'https://picsum.photos/700/250/?image=23',
         'https://picsum.photos/700/250/?image=24',
         'https://picsum.photos/700/250/?image=26',
-        'https://picsum.photos/700/250/?image=27',
-        'https://picsum.photos/700/250/?image=28',
         'https://picsum.photos/700/250/?image=41',
-        'https://picsum.photos/700/250/?image=61'
+        'https://picsum.photos/700/250/?image=28',
+        'https://picsum.photos/700/250/?image=21',
+        'https://picsum.photos/700/250/?image=20'
     ];
     public config: Object = {
         paginationClickable: true,
@@ -42,8 +42,27 @@ export class AppComponent {
         spaceBetween: 0,
         zoom: true
     };
+    public config_thumbs_vertical: Object = {
+        direction: 'vertical',
+        loop: true,
+        thumbs: this.images,
+        slidesPerView: 3.5,
+        spaceBetween: 10,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    };
+    public config_gallery_vertical: Object = {
+        lazyLoading: true,
+        loop: true,
+        spaceBetween: 0,
+        zoom: true
+    };
 
     @ViewChild('usefulSwiper') public usefulSwiper: SwiperComponent;
     @ViewChild('swiperThumbs') public swiperThumbs: SwiperComponent;
     @ViewChild('swiperGallery') public swiperGallery: SwiperComponent;
+    @ViewChild('swiperThumbs_vertical') public swiperThumbs_vertical: SwiperComponent;
+    @ViewChild('swiperGallery_vertical') public swiperGallery_vertical: SwiperComponent;
 }
